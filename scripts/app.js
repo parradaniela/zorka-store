b16eacb2bf355a1498e6dcc21e7b0f4e073dc687
DanielaParra <dparralp396@gmail.com>
2023-02-15 20:44:11 -0500

    Added proper namespacing, and created reusable toggleActive function to use for header and footer buttons

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,27 @@
-const accordionItem = document.querySelectorAll('.accordion-item');
-console.log(accordionItem);
+const zorkaApp = {};
 
-accordionItem.forEach((item) => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('active');
+zorkaApp.toggleActiveClass = (toggleTrigger, toggledElement) => {
+    toggleTrigger.addEventListener('click', () => {
+        toggledElement.classList.toggle('active');
+    });
+}
+
+zorkaApp.handleMobileNavToggle = () => {
+    const navToggleButton = document.querySelector('.header__toggle');
+    const navLinksMenu = document.querySelector('.header__mobile-fixed');
+    zorkaApp.toggleActiveClass(navToggleButton, navLinksMenu)
+}
+
+zorkaApp.handleFooterListsToggle = () => {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+    accordionItems.forEach((dropdownItem) => {
+        zorkaApp.toggleActiveClass(dropdownItem, dropdownItem);
     })
-})
+}
+
+zorkaApp.init = () => {
+    zorkaApp.handleMobileNavToggle();
+    zorkaApp.handleFooterListsToggle();
+}
+
+zorkaApp.init();
